a5349038c776b66bb86c1ea1a47f94f67071c575
Kabir <kabir4691@gmail.com>
2020-03-17 20:40:01 +0530

    Add logic for handling conversions in controller and api call

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,47 +6,125 @@
 *       
 */
 
+const impUnits = ['gal', 'lbs', 'mi'];
+const metUnits = ['L', 'kg', 'km'];
+
 function ConvertHandler() {
   
+  /**
+   * Returns the number from the given input.
+   * 
+   * @param input The input string
+   * @returns The parsed number. If the number is not specified, returns 1. If number is specified but is invalid, returns null.
+   *  */ 
   this.getNum = function(input) {
-    var result;
+    const index = input.split('').findIndex(char => isLetter(char));
+    let numberStr = index === -1 ? input : input.slice(0, index);
     
-    return result;
+    if (numberStr.length === 0 ) return 1;
+    
+    const split = numberStr.split('/');
+    if (split.length > 2) return null;    
+    // Check if does not contain valid numbers
+    if (split.some(item => isNaN(item))) return null;
+
+    return split.map(Number).reduce((a, b) => a / b);
   };
   
+  /**
+   * Returns the unit from the given input.
+   * 
+   * @param input The input string
+   * @returns The unit. If its invalid or not mentioned, returns null.
+   *  */ 
   this.getUnit = function(input) {
-    var result;
-    
-    return result;
+    const index = input.split('').findIndex(char => isLetter(char));
+    if (index === -1) return null;
+    const unit = input.slice(index);
+    if (impUnits.map(item => item.toLowerCase()).includes(unit.toLowerCase())
+    || metUnits.map(item => item.toLowerCase()).includes(unit.toLowerCase())) {
+      return unit;
+    }
+    return null;
   };
   
+  /**
+   * Returns the corresponding unit (imperial to metric or vice versa).
+   * 
+   * @param initUnit The input unit
+   * @returns The corresponding output unit. If input unit is invalid, returns null.
+   */
   this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+    let index = impUnits.map(unit => unit.toLowerCase()).indexOf(initUnit.toLowerCase());
+    if (index !== -1) {
+      return metUnits[index];
+    }
+    index = metUnits.map(unit => unit.toLowerCase()).indexOf(initUnit.toLowerCase());
+    if (index !== -1) {
+      return impUnits[index];
+    }
+    return null;
   };
 
+  /** 
+   * Returns the complete string of a unit
+   * 
+   * @param unit The unit's shorthand
+   * @returns The unit's full text
+   */
   this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
+    switch(unit.toLowerCase()) {
+      case 'gal': return 'gallon(s)';
+      case 'lbs': return 'pound(s)';
+      case 'mi': return 'mile(s)';
+      case 'l': return 'litre(s)';
+      case 'kg': return 'kilogram(s)';
+      case 'km': return 'kilometre(s)';
+    }
+    return null;
   };
   
+  /**
+   * Converts a value into its corresponding value in other system i.e. metric to imperial or vice versa.
+   * 
+   * @param initNum The input value
+   * @param initUnit The unit of the input value
+   * @returns the value after conversion to other system
+   * 
+   */
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
-    return result;
+
+    switch(initUnit.toLowerCase()) {
+      case 'gal': return initNum * galToL;
+      case 'lbs': return initNum * lbsToKg;
+      case 'mi': return initNum * miToKm;
+      case 'l': return initNum / galToL;
+      case 'kg': return initNum / lbsToKg;
+      case 'km': return initNum / miToKm;
+    }
+    return null;
   };
   
+  /**
+   * Returns a output response of a conversion in the form of an object
+   * 
+   * @param initNum the input value
+   * @param initNum the unit of the input value
+   * @param returnNum the converted value
+   * @param returnUnit the unit of the converted value
+   * @returns The output object
+   */
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
+    const string = `${initNum} ${spellOutUnit(initUnit)} converts to ${returnNum} ${spellOutUnit(returnUnit)}`;
+    return { initNum, initUnit, returnNum, returnUnit, string };
   };
   
 }
+
+/** Check if character is a letter (upper or lower case) */
+const isLetter = char => /[a-zA-Z]/.test(char);
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,19 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
+      const input = req.query.input;
+      
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
+
+      if (initNum === null && initUnit === null) return res.send('invalid number and unit');
+      if (initNum === null) return res.send('invalid number');
+      if (initUnit === null) return res.send('invalid unit');
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
-      //res.json
+      res.status(200).json(toString);
     });
     
 };

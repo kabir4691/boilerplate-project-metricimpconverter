2e4f074a2165a8263129ec95ed2b841f57560f94
Kabir <kabir4691@gmail.com>
2020-03-17 12:48:42 +0530

    Add helmet middleware to prevent MIME sniffing and XSS attacks

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+const helmet = require('helmet');
+
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +19,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter({}));
 
 //Index page (static HTML)
 app.route('/')
